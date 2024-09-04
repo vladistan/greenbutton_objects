f544e97d617b4462caf74ce10ceb8bec0ae64811
Vlad Korolev <vlad@v-lad.org>
2024-09-04 15:39:41 -0400

    Fixup start script a bit

--- .devcontainer/with_energy_app/env_setup.sh
+++ .devcontainer/with_energy_app/env_setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+
+STARTUP_DIR=$(pwd)
+
+python -m pip install -U pip
+
 # Clone home energy app
-git clone https://github.com/vladistan/home-energy-analysis-tool.git ../home-energy-analysis-tool
+if [ ! -d "../home-energy-analysis-tool" ]; then
+  git clone https://github.com/vladistan/home-energy-analysis-tool.git ../home-energy-analysis-tool
+fi
 
 # Set up the environment for the home energy app
 cd ../home-energy-analysis-tool/rules-engine
-pip install -e .
-pip install -r requirements-dev.txt
+python -m pip install -r requirements.txt
+python -m pip install -r requirements-dev.txt
+python -m pip install -e .
 
-Run the setup script from default container
+# Run the setup script from default container
 echo "----------------- Setting up Greenbutton library envrionment -------"
+cd $STARTUP_DIR
 .devcontainer/env_setup.sh
