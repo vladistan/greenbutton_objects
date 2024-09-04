05068835e45bddb356499de5210047a6779d7d1c
Vlad Korolev <vlad@v-lad.org>
2024-09-03 21:28:43 -0400

    WIP Saving progress

---
+++ .devcontainer/with_energy_app/env_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone home energy app
+#git clone https://github.com/vladistan/home-energy-analysis-tool.git ../home-energy-analysis-tool
+
+# Set up the environment for the home energy app
+cd ../home-energy-analysis-tool/rules-engine
+pip install -e .
+pip install -r requirements-dev.txt
+
+# Run the setup script from default container
+# echo "----------------- Setting up Greenbutton library envrionment -------"
+# .devcontainer/env_setup.sh
